79fbf1c2bde7b5ee943083ba5d4d6b4800db211d
pengwenyu <1031781561@qq.com>
2020-01-31 14:46:56 -0800

    second commit

--- public/App.js
+++ public/App.js
@@ -1,4 +1,4 @@
 const element = React.createElement("div", {
   title: "Outer div"
-}, React.createElement("p", null, "I want to get A in this class. So I will work hard for that. Sorry to bother you if I want to know more about my work."));
+}, React.createElement("p", null, "I want to get A in this class. So I will work hard for that. Sorry to bother you if I want to know more about my work."), React.createElement("p", null, "test"));
 ReactDOM.render(element, document.getElementById('contents'));
